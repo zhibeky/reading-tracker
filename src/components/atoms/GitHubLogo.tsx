ff94effd073dcbac50487d88cc7975a0a7a0a759
Zhibek Zhagypar <zhibekazhibeka@gmail.com>
2025-02-07 12:13:35 +0000

    Handle errors in svg

--- src/components/atoms/GitHubLogo.tsx
+++ src/components/atoms/GitHubLogo.tsx
@@ -14,18 +14,18 @@ export const GitHubLogo = () => {
                 viewBox="0,0,256,256">
                 <g
                     fill="#ffffff"
-                    fill-rule="nonzero"
+                    fillRule="nonzero"
                     stroke="none"
-                    stroke-width="1"
-                    stroke-linecap="butt"
-                    stroke-linejoin="miter"
-                    stroke-miterlimit="10"
-                    stroke-dasharray=""
-                    stroke-dashoffset="0"
-                    font-family="none"
-                    font-weight="none"
-                    font-size="none"
-                    text-anchor="none"
+                    strokeWidth="1"
+                    strokeLinecap="butt"
+                    strokeLinejoin="miter"
+                    strokeMiterlimit="10"
+                    strokeDasharray=""
+                    strokeDashoffset="0"
+                    fontFamily="none"
+                    fontWeight="none"
+                    fontSize="none"
+                    textAnchor="none"
                    >
                     <g transform="scale(8.53333,8.53333)">
                         <path
