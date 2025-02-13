c1b4476e394e3c28689cc81759eef27ecba38bdf
Zhibek Zhagypar <zhibekazhibeka@gmail.com>
2025-02-13 14:37:47 +0000

    StatsPage fixes

--- src/components/pages/StatsPage.tsx
+++ src/components/pages/StatsPage.tsx
@@ -1,9 +1,9 @@
 import {FC} from "react";
 export const StatsPage: FC = () => {
     return (
-        <div className="p-6">
+        <div className="p-6 max-w-5xl mx-auto bg-white rounded-lg shadow-md text-center">
             <h1 className="text-2xl font-bold">Stats Page</h1>
-            <p className="mt-4">Visualize your app's analytics here!</p>
+            <p className="mt-4">Your app's analytics will be visualized here!</p>
         </div>
     );
 };
